6e4b284ab464f79eba4ceaee86167a3ed0c96962
GonzaloSaad <saad.gonzalo.ale@gmail.com>
2021-05-04 23:56:09 -0300

    Add ej 11

---
+++ src/exercise/ej_011.py
@@ -0,0 +1,23 @@
+from templates import equalization
+
+#######
+# Inputs
+#######
+grey_scale = 15
+
+matrix_str = """
+8 5 5 5 8 8 5 5 5 8
+5 2 2 5 5 5 2 2 5 8
+5 4 4 2 5 2 5 2 5 8
+5 4 4 4 2 5 5 5 5 8
+5 5 4 4 4 5 5 5 5 8
+8 5 5 4 4 5 5 5 5 8
+8 8 5 5 5 5 5 8 8 8
+8 8 8 8 8 8 8 8 8 8
+"""
+
+#######
+# Solution
+#######
+if __name__ == '__main__':
+    equalization.resolve(matrix_str, grey_scale)
